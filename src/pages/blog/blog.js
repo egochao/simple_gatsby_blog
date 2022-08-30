0a1385cb2e371314414035b9a4f6f616357a84ce
egochao <johnoldmanworkspace@gmail.com>
2022-08-30 04:48:24 +0000

    get /blog running

---
+++ src/pages/blog/blog.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import Layout from '../../components/Layout'
+import Seo from '../../components/seo'
+import { graphql } from 'gatsby'
+
+
+const BlogPage = ( { data } ) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+        {
+            data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <p>{node.excerpt}</p>
+            </article>
+            ))
+        }
+
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query MyQuery {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+        frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
+        }
+        id
+        excerpt
+        }
+    }
+    }
+`
+
+export const Head = () => <Seo title="My Blog Posts" />
+
+export default BlogPage
